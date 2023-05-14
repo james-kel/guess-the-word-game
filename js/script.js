a98fe4114f20e80839469ffe468b15bf2afda239
james_k <jamesk91@live.co.uk>
2023-05-14 18:57:57 +0100

    Added validateInput and makeGuess functions

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 //The unordered list where the player’s guessed letters will appear.
-const guessedLetters = document.querySelector(".guessed-letters");
+const guessedLettersElement = document.querySelector(".guessed-letters");
 //The button with the text “Guess!” in it.
 const guessLetterButton = document.querySelector(".guess");
 //The text input where the player will guess a letter.
@@ -16,6 +16,7 @@ const message = document.querySelector(".message");
 const playAgain = document.querySelector(".play-again");
 
 const word = "magolia";
+const guessedLetters = [];
 
 const placeholder = function (word) {
     const placeholderLetters = [];
@@ -30,8 +31,38 @@ placeholder(word);
 
 guessLetterButton.addEventListener = ("click", function (e) {
     e.preventDefault();
+    message.innerText = "";
     const guess = letterInput.value;
-    console.log(guess);
-    letterInput.value = "";
+    const goodGuess = validateInput(guess);
+
+  if (goodGuess) {
+    // We've got a letter! Let's guess!
+    makeGuess(guess);
+  }
+  letterInput.value = "";
 });
+
+const validateInput = function (input) {
+    const acceptedLetter = /[a-zA-Z]/;
+    if (input.lenght === 0) {
+        message.innerText = "Please enter a letter";
+    } else if (input > 1) {
+        message.innerText = "please enter a single letter";
+    } else if (!input.match(acceptedLetter) ) {
+        message.innerText = "please enter a letter from a to z";
+    } else {
+        //console.log("guessed letter")
+        return input;
+    }
+};
+
+const makeGuess = function (guess) {
+    guess = guess.toUpperCase();
+    if (guessedLetters.includes(guess)) {
+      message.innerText = "You already guessed that letter, silly. Try again.";
+    } else {
+      guessedLetters.push(guess);
+      console.log(guessedLetters);
+    }
+  };
 
